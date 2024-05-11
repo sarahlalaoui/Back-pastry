734a3209ca6c532c1e270c0c590be18d66b5becf
sarahlalaoui <lalaouisarah002@hotmail.com>
2024-05-12 00:53:22 +0100

    pastry

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const postController = require("../controllers/postControllers");
 const {imageUpload} = require("../middlewares/middlupload").
 
-router.post("/",imageUpload , postController.createPost);
+router.post("/",postController.createPost)
 router.get("/", postController.getAllPosts);
 router.delete("/:id", postController.deletePost);
 router.put("/:id", imageUpload, postController.updatePost);
--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const authRoute = require('./routes/authroute')
-
+const postRoute = require('./routes/postRoutes')
 
 app.use(express.json())
 
 app.use(cors())
 // routes:
 app.use('/auth', authRoute)
+app.use('/product', postRoute)
 
 
 // server init:
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port , ()=> console.log(`server running on port ${port}`))
 
 // connecting to database:
